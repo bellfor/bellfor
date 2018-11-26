93997e74f15c1d2c45a3c9ac5664dda754d07463
SpitF1re <kostyaskiller@gmail.com>
2018-11-26 12:36:05 +0200

    СрфÑChanged the masage about the addition of goods UK

--- catalog/language/en-gb/checkout/cart.php
+++ catalog/language/en-gb/checkout/cart.php
@@ -3,7 +3,7 @@
 $_['heading_title']            = 'Shopping Cart';
 
 // Text
-$_['text_success']             = 'Success: You have added <a href="%s">%s</a> was added successfully.';
+$_['text_success']             = '<a href="%s">%s</a> was added successfully.';
 $_['text_remove']              = 'Success: You have modified your shopping cart!';
 $_['text_login']               = 'Attention: You must <a href="%s">login</a> or <a href="%s">create an account</a> to view prices!';
 $_['text_items']               = '%s item(s) - %s';
